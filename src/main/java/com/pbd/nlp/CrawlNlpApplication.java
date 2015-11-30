c2cb7b066c6b18ed64060f2f4a16608220d109cf
neo_microkid <neo_microkid@yahoo.co.uk>
2015-12-01 00:39:09 +0700

    init

---
+++ src/main/java/com/pbd/nlp/CrawlNlpApplication.java
@@ -0,0 +1,12 @@
+package com.pbd.nlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlNlpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlNlpApplication.class, args);
+    }
+}
